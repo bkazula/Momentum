459705dc10f02a1d63e071e41530bb669e608fb8
Bartosz <bartosz@kazula.eu>
2022-06-14 19:16:49 +0200

    Added field to topic with the second speaker

--- packages/database/migrations/1655167446676_alter_table_public_session_add_column_second_speaker_id/down.sql
+++
@@ -1,4 +0,0 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- alter table "public"."session" add column "second_speaker_id" uuid
---  null;
