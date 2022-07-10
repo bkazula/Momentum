f3153876f8b118a7528c6335d1652a70857fb596
Bartosz <bartosz@kazula.eu>
2022-07-10 21:40:38 +0200

    Open navigation in maps app

--- packages/app/src/screens/TabsNavigation/screens/Map/utils/systemNavigate.ts
+++ packages/app/src/screens/TabsNavigation/screens/Map/utils/systemNavigate.ts
@@ -2,34 +2,37 @@ import { Linking, Platform } from 'react-native'
 
 const navigate = (lat: number, lng: number, title?: string): void => {
   const latLng = `${lat},${lng}`
-  const label = 'Custom Label'
+  const iosLink = `maps:0,0?q=${title}@${latLng}`
   const url = Platform.select({
-    ios: `maps:0,0?q=${label}@${latLng}`,
-    android: `geo:0,0?q=${latLng}(${label})`,
+    ios: iosLink,
+    android: `geo:0,0?q=${latLng}(${title})`,
     web: `https://www.google.com/maps/dir//${title ? encodeURI(title) : latLng}/@${latLng},13z`,
   })
-
-  if (Platform.OS === 'web') {
-    if (/(android)/i.test(navigator.userAgent)) {
-      Linking.openURL(`geo:0,0?q=${title ? title.replace(' ', '+') : latLng}(${title})`)
-      return
+  ;(async () => {
+    if (Platform.OS === 'web') {
+      if (/(android)/i.test(navigator.userAgent)) {
+        Linking.openURL(`geo:0,0?q=${title ? title.replace(' ', '+') : latLng}(${title})`)
+        return
+      }
+      if (
+        ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(
+          navigator.platform,
+        ) ||
+        (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
+      ) {
+        if (await Linking.canOpenURL(iosLink)) {
+          Linking.openURL(iosLink)
+          return
+        }
+        Linking.openURL(`http://maps.apple.com/?daddr=${latLng}&ll=${latLng}`)
+        return
+      }
     }
-    if (
-      ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'].includes(
-        navigator.platform,
-      ) ||
-      (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
-    ) {
-      Linking.openURL(
-        `http://maps.apple.com/?daddr=${title ? title.replace(' ', '+') : latLng}&ll=${latLng}`,
-      )
-      return
-    }
-  }
 
-  if (url) {
-    Linking.openURL(url)
-  }
+    if (url) {
+      Linking.openURL(url)
+    }
+  })()
 }
 
 export default navigate
